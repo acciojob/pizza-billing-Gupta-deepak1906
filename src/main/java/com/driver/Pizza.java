195e0c08fce6dda0d86ea2f74b79c26d671ac6b5
Gupta-deepak1906 <gdeepakg9997@gmail.com>
2024-03-11 00:17:56 +0530

    Message1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,20 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    boolean isAddExtraCheese;
+    boolean isAddExtraTopping;
+    boolean isTakeaway;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price=300;
+        }
+        else if(!isVeg){
+            this.price=400;
+        }
+
     }
 
     public int getPrice(){
@@ -17,14 +27,28 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isAddExtraCheese){
+            price+=80;
+            isAddExtraCheese=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isAddExtraTopping && isVeg){
+            price+=70;
+        }else if(!isAddExtraTopping && !isVeg){
+            price+=120;
+        }
+        isAddExtraTopping=true;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeaway){
+            price+=20;
+        }
+        isTakeaway=true;
     }
 
     public String getBill(){
